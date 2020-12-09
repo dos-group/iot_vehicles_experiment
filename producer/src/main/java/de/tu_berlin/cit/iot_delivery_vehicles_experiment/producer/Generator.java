1e12d0b89bd4837100a2f8d9779c44c15f367b46
Morgan Karl Geldenhuys <morgan.geldenhuys@tu-berlin.de>
2020-12-09 20:42:27 +0100

    capacity testing

--- producer/src/main/java/de/tu_berlin/cit/iot_delivery_vehicles_experiment/producer/Generator.java
+++ producer/src/main/java/de/tu_berlin/cit/iot_delivery_vehicles_experiment/producer/Generator.java
@@ -35,7 +35,8 @@ public enum Generator { GET;
         System.out.println(ts.observations.size());
         for (int i = 0; i < ts.observations.size(); i++) {
             // test if new vehicles need to be created
-            int vehiclesNeeded = ts.observations.get(i).value - VehicleActor.VEHICLE_COUNT.get();
+            //int vehiclesNeeded = ts.observations.get(i).value - VehicleActor.VEHICLE_COUNT.get();
+            int vehiclesNeeded = 100000 - VehicleActor.VEHICLE_COUNT.get();
             if (vehiclesNeeded > 0) {
                 // create the desired number of vehicles
                 IntStream.range(0, vehiclesNeeded).forEach(j -> {
